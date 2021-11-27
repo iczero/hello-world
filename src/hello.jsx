430e77c592c34c7980383bc25a4e7bb024397720
iczero <iczero4@gmail.com>
2021-11-26 19:14:33 -0500

    RAINBOWS

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Buffer } from 'buffer';
 import { Keccak, KeccakRand } from './keccak';
+import { Head } from './util';
 
 export let keccak = new Keccak(12);
 export let keccakRand = new KeccakRand(keccak, 512);
@@ -36,11 +37,30 @@ export function exclamify(a) {
 }
 
 export default () => <>
-  <div className="hello">{exclamify('Hello, world')}</div>
-  <KeccakButton />
-  <style jsx>{`
-    .hello {
-      color: red;
-    }
-  `}</style>
+  <Head>
+    <title>Hello, world!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </Head>
+  <>
+    <div className="hello">{exclamify('Hello, world')}</div>
+    <KeccakButton />
+    <style jsx>{`
+      .hello {
+        font-size: 2em;
+        font-family: sans-serif;
+        animation: rainbow 10s infinite;
+      }
+
+      @keyframes rainbow {
+        0% { color: red; }
+        14.28% { color: orange; }
+        28.57% { color: yellow; }
+        42.86% { color: green; }
+        57.14% { color: blue; }
+        71.43% { color: indigo; }
+        85.71% { color: violet; }
+        100% { color: red; }
+      }
+    `}</style>
+  </>
 </>;
